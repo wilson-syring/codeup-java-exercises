51b283a8e73c839d6f4f91fecc04c1dc98b8f2fe
Wilson Syring <wilson.syring@gmail.com>
2022-11-09 17:24:30 -0600

    completed StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String noEd = "We don't need no education\nWe don't need no thought control";
+        System.out.println(noEd);
+        System.out.println();
+
+        String checkIt = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkIt);
+        System.out.println();
+
+        String window = "In windows, the main drive is usually C:\\";
+        System.out.println(window);
+        System.out.println();
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
+        System.out.println();
+    }
+}
