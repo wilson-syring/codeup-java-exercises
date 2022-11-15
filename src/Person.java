3c1b1df226d0ad2e1739ccd4cdfd6bfe29fd636c
Wilson Syring <wilson.syring@gmail.com>
2022-11-15 12:26:51 -0600

    completed the person class exercise

---
+++ src/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Yo, how you doing " + name + "?");
+    }
+
+    public static void main(String[] args) {
+        Person wilson = new Person("Wilson");
+        System.out.println("wilson.getName() = " + wilson.getName());
+        wilson.setName("Wilson");
+        wilson.sayHello();
+
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+    }
+}
