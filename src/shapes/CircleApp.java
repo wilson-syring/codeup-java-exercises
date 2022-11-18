465702a3729a50015b3c1421a2ba5cc42a0497ec
Wilson Syring <wilson.syring@gmail.com>
2022-11-18 13:41:30 -0600

    refactored circle app to use yesNo

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -7,10 +7,17 @@ import java.util.Scanner;
 public class CircleApp {
     public static void main(String[] args) {
         Input myInput = new Input(new Scanner(System.in));
-        System.out.print("Please enter a radius:");
-        var radius = myInput.getDouble();
-        Circle myCircle = new Circle(radius);
-        System.out.printf("Area = %.2f\n" , myCircle.getArea());
-        System.out.printf("Circumference = %.2f" , myCircle.getCircumference());
+        boolean circleAgain = true;
+        System.out.println("Would you like to play the circle game?");
+        circleAgain = myInput.yesNo();
+        while (circleAgain) {
+            System.out.print("Please enter a radius:");
+            var radius = myInput.getDouble();
+            Circle myCircle = new Circle(radius);
+            System.out.printf("Area = %.2f\n", myCircle.getArea());
+            System.out.printf("Circumference = %.2f\n", myCircle.getCircumference());
+            System.out.println("want to go around again?");
+            circleAgain = myInput.yesNo();
+        }
     }
 }
