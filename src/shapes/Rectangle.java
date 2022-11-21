36e2da0a686408b680e71a3832cdb8944dcb27b8
Wilson Syring <wilson.syring@gmail.com>
2022-11-21 14:01:51 -0600

    completed square

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -4,6 +4,11 @@ public class Rectangle {
     protected double length;
     protected double width;
 
+    public Rectangle(double length, double width) {
+        this.width = width;
+        this.length = length;
+    }
+
     public double getArea(){
         return this.length * this.width;
     }
