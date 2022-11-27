9720a60917de6886f0104a4bc4fc3c38665fd669
Wilson Syring <wilson.syring@gmail.com>
2022-11-27 13:13:36 -0600

    created student blueprint

---
+++ src/grades/Student.java
@@ -0,0 +1,37 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList grade = new ArrayList();
+    Student(){}
+    Student(String name) {
+        this.name = name;
+    }
+    public ArrayList getGrade() {
+        return this.grade;
+    }
+    public int getGrade(int grade) {
+        return (int)this.grade.get(grade);
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grade.add(grade);
+    }
+
+    public int getArrSize() {
+        return this.grade.size();
+    }
+
+    public double getGradeAverage() {
+        double total = 0;
+        for (int g = 0; g < grade.size(); g++) {
+            total += ((double) (int) this.grade.get(g));
+        }
+        return total / this.grade.size();
+    }
+}
